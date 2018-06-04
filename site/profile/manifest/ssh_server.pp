c0885e62be2308140ac60cf0b742627b553523ba
titodml <37911228+titodml@users.noreply.github.com>
2018-06-04 10:21:18 -0500

    Update ssh_server.pp

--- site/profile/manifest/ssh_server.pp
+++ site/profile/manifest/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCh7SZVeM55P5x1MuFyw0N73CcWZlAPd8HAALhEmrMZfvbrenKgfhiJVx3lCLAYhLQD10ZTlDYSg2mqDD0ZAoR/wRCDdMOpMdU5SGJJv9P6dAOlsUNiJH5tI6hASeAUzIEVQS3PzgsXjLTGPX7fiVemUnu69DDLCynRzspSYQppso529q1BP33/ohr9J1xcrZohn+zsP+OkPxoEfiXYx8ROyvBqgOf5bpZweJ3OiTO/pQt42qqBhIUQrhpxlUJVfnibjW8Taxshd/jZR6Lg3ZLYl8Pc2pnpuGUVZT0jDW+YNtqtI6EK2UvF5EOyneY8MjVkbr0uydJQs9KZwzygdYuf',
 	}  
 }
